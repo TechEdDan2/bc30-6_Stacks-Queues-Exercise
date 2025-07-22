ff7e52e6773dfba02fa056ada0658e90c40674c4
DNel2 <dnel2@Dannys-MacBook-Air.local>
2025-07-22 14:36:24 -0400

    The Queues Class is complete with all functions implimented

--- queue.js
+++ queue.js
@@ -20,6 +20,17 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    const newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+
+    return undefined;
 
   }
 
@@ -27,19 +38,40 @@ class Queue {
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (this.size === 0) {
+      throw new Error("Cannot dequeue from an empty queue");
+    }
 
+    const removedNode = this.first;
+    if (this.last === this.first) {
+      this.last = null;
+      this.first = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.size--;
+
+    return removedNode.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
+    if (this.size === 0) {
+      throw new Error("This is an empty queue, cannot peek at the first element");
+    }
+    return this.first.val;
 
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
